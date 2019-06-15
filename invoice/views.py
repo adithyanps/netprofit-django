075ca00d030ecd807884af9148efc9382c3fa05b
ADITHYAN <adithynps3@gmial.com>
2019-06-15 18:39:33 +0530

    completd upto create journal entries

--- invoice/views.py
+++ invoice/views.py
@@ -8,7 +8,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import viewsets, mixins, status
 from rest_framework import views
-from rest_framework import filters
+# from rest_framework import filters
 from itertools import zip_longest as zip
 from collections import defaultdict
 from django.http import JsonResponse
@@ -21,6 +21,11 @@ from invoice import serializers
 from . import Permissions
 from rest_framework.generics import RetrieveAPIView
 from rest_framework_serializer_extensions.views import SerializerExtensionsAPIViewMixin
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter,SearchFilter
+from django_filters import FilterSet
+import django_filters
+from django_filters import rest_framework as filters
 
 # Create your views here.
 class CustomerViewset(viewsets.ModelViewSet):
@@ -29,8 +34,8 @@ class CustomerViewset(viewsets.ModelViewSet):
     serializer_class = serializers.CustomerSerializer
 
     """search json data on api"""
-    filter_backends = (filters.SearchFilter,)
-    search_fields = ("name",)
+    # filter_backends = (filters.SearchFilter,)
+    # search_fields = ("name",)
 
     def get_queryset(self):
         """Return objects for the current authenticated user only"""
@@ -43,8 +48,8 @@ class BranchViewset(viewsets.ModelViewSet):
     serializer_class = serializers.BranchSerializer
 
     """filter option on api"""
-    filter_backends = (filters.SearchFilter,)
-    search_fields = ("branch",)
+    # filter_backends = (filters.SearchFilter,)
+    # search_fields = ("branch",)
 
     def get_queryset(self):
         """return objects"""
@@ -56,8 +61,8 @@ class ItemViewset(viewsets.ModelViewSet):
     queryset = models.Item.objects.all()
     serializer_class = serializers.ItemsSerializer
 
-    filter_backends = (filters.SearchFilter,)
-    search_fields = ("item",)
+    # filter_backends = (filters.SearchFilter,)
+    # search_fields = ("item",)
 
     def get_queryset(self):
         """Return objects for the current authenticated user only"""
@@ -69,8 +74,10 @@ class AccountViewset(viewsets.ModelViewSet):
     queryset = models.Account.objects.all()
     serializer_class = serializers.AccountSerializer
 
-    filter_backends = (filters.SearchFilter,)
-    search_fields = ("type",)
+    # filter_backends = (filters.SearchFilter,)
+    # search_fields = ("type",)
+    # filter_fields = ("type",)
+
 
     def get_queryset(self):
         return self.queryset.order_by('id')
@@ -91,10 +98,32 @@ class JournalEntryViewset(viewsets.ModelViewSet):
     def get_queryset(self):
         return self.queryset.order_by('id')
 
+class SalesInvoiceFilter(FilterSet):
+    # customer = CharFilter('customer')
+    start_date = filters.DateFilter(method="filter_by_start_date")
+    end_date = filters.DateFilter(method="filter_by_end_date")
+
+    class Meta:
+        model = models.SalesInvoice
+        fields = "__all__"
+        # fields = ["customer","date"]
+
+    def filter_by_start_date(self, queryset, name, value):
+        queryset = queryset.filter(date__gte = value)
+        return queryset
+
+    def filter_by_end_date(self, queryset, name, value):
+        queryset = queryset.filter(date__lte = value)
+        return queryset
+
 
 class SalesInvoiceViewSet(viewsets.ModelViewSet):
     queryset = models.SalesInvoice.objects.all()
     serializer_class = serializers.ParantInvoiceSerializerTest
+    filter_backends = (DjangoFilterBackend,OrderingFilter, SearchFilter)
+    filter_class = SalesInvoiceFilter
+    search_fields = ('date',"customer")
+
 
     def get_queryset(self):
         return self.queryset.order_by('id')
@@ -106,6 +135,7 @@ class ParentInvoiceViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         return self.queryset.order_by('id')
+
 
 
 class InvoiceLineViewSet(viewsets.ModelViewSet):
@@ -155,9 +185,32 @@ class ExpenseCategoryViewSet(viewsets.ModelViewSet):
         return self.queryset.order_by('id')
 
 
+class ExpenseFilter(FilterSet):
+    # customer = CharFilter('customer')
+    start_date = filters.DateFilter(method="filter_by_start_date")
+    end_date = filters.DateFilter(method="filter_by_end_date")
+
+    class Meta:
+        model = models.Expenses
+        fields = "__all__"
+        # fields = ["customer","date"]
+
+    def filter_by_start_date(self, queryset, name, value):
+        queryset = queryset.filter(Date__gte = value)
+        return queryset
+
+    def filter_by_end_date(self, queryset, name, value):
+        queryset = queryset.filter(Date__lte = value)
+        return queryset
+
+
 class ExpenseViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.ExpenseSerializer
     queryset = models.Expenses.objects.all()
+
+    filter_backends = (DjangoFilterBackend,OrderingFilter, SearchFilter)
+    filter_class = ExpenseFilter
+    search_fields = ('Date',"ExpenseCategory","ExpenseAcct")
 
     def get_queryset(self):
         return self.queryset.order_by('id')
